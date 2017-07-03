3d8f1100984aa312cbbf1d25f00665982dca0ed1
Alex Viytiv <sviytiv@gmail.com>
2017-07-03 13:31:13 +0300

    Code format corrections & code optimizaton.
    Particular lines of code were corrected due to comments in previous commit. 
    Main changes : 
     - Renamed variables & functions.
     - Optimized code lines, where present work with files.
     - Added docstrings.

--- file.py
+++ file.py
@@ -8,97 +8,89 @@ Syntax of run command : python3 <name_of_program> <name_of_packets_stack>
 import sys
 
 # Receivers
-IVASYK = "Ivasyk.txt"
-DMYTRYK = "Dmytryk.txt"
-OSTAP = "Ostap.txt"
-LESYA = "Lesya.txt"
+IVASYK = "Ivasyk"
+DMYTRYK = "Dmytryk"
+OSTAP = "Ostap"
+LESYA = "Lesya"
 # Sign values used to detect receiver.
 IVASYK_SIGN_VAL = 0
 LESYA_SIGN_VAL = "end"
 
-# Function dictToFile() used to write content of dictionary values
-# to files with key as name of file.
+
+def packets_to_file(receivers):
+    """
+     Function packets_to_file() used to write content of dictionary values
+     to files with key as name of file.
+    """
+    for receiver in receivers:
+        with open("{}.txt".format(receiver), "w") as file:
+            for packet in receivers[receiver]:
+                file.write("{}\n".format(packet))
 
 
-def dictToFile(Dict):
-    for key in Dict:
-        file = open(key + ".txt", "w")
-        # line - represents each packet
-        for line in Dict[key]:
-            file.write(line + "\n")
-        file.close()
-
-# Function parsePacketReceiver() used to classify received packets.
-# Packet represented by lines.
-
-
-def parsePacketReceiver(lines, receivers):
-    # line - represents each packets
-    for line in lines:
-        # remove newline symbols
-        line = line.strip('\n')
-        # drop empty packet as redudant
-        if(len(line) == 0):
+def clasify_packets(packets, receivers):
+    """
+     Function clasify_packets() used to classify received packets.
+     Packet represented by lines.
+    """
+    for packet in packets:
+        packet = packet.strip('\n')
+        if(len(packet) == 0):
             continue
 
-        # begins classification
         # if packet contain even number of symbols - it`s Ivasyk
-        if(len(line) % 2 == IVASYK_SIGN_VAL):
-            (receivers[IVASYK]).append(line)
-        # if packet first letter is capitalized and it`s not Ivasyk
+        if(len(packet) % 2 == IVASYK_SIGN_VAL):
+            (receivers[IVASYK]).append(packet)
+
+        # if packet first letter is capitalized and it`s not for Ivasyk
         # - it`s Dmytryk
-        elif(line[0].isupper()):
-            (receivers[DMYTRYK]).append(line)
+        elif(packet[0].isupper()):
+            (receivers[DMYTRYK]).append(packet)
+
         # if packet is not for Lesya && not for anyone - it`s Ostap
-        elif(line[-3:] != LESYA_SIGN_VAL):
-            (receivers[OSTAP]).append(line)
+        elif(packet[-len(LESYA_SIGN_VAL):] != LESYA_SIGN_VAL):
+            (receivers[OSTAP]).append(packet)
 
         # if packet last letters is LESYA_SIGN_VAL - it`s Lesya
-        if(line[-len(LESYA_SIGN_VAL):] == LESYA_SIGN_VAL):
-            (receivers[LESYA]).append(line)
-
-# Function openPacketsStack() used to open file that contain all
-# received packets. If something goes wrong, it print message to inform
-# user about possible problem.
+        if(packet[-len(LESYA_SIGN_VAL):] == LESYA_SIGN_VAL):
+            (receivers[LESYA]).append(packet)
 
 
-def openPacketsStack(filePath):
+def open_logs(filePath):
+    """
+     Function open_logs() used to open file that contain all
+     received packets. If something goes wrong, it print message to inform
+     user about possible problem.
+    """
     try:
         # attemp to open stack of IDOL packets
-        file = open(filePath, "r")
+        with open(filePath, "r") as file:
+            packets = file.readlines()
     except FileNotFoundError:
         print(" [Problem] | File doesn`t exist. Please, check file name.")
         exit(1)
     except Exception:
-        print(""" [Problem] | Unknown type of problem. Please, send this information
-                              to idolprotocol_support@gmail.com .""")
+        print(""" [Problem] | Unknown type of problem. Please, send this
+                              information to idolprotocol_support@gmail.com .""")
         exit(1)
 
-    # temporary save all packets in list.
-    # TIP : it would be better to directly return file.readlines(),
-    #       but we need to close file =(
-    lines = file.readlines()
-    file.close()
-    return lines
-
-# Function main() realize calls of all functions.
+    return packets
 
 
 def main():
-    # dictionary of possible receivers
+    """
+     Function main() realize calls of all functions.
+    """
     receivers = {IVASYK: [], DMYTRYK: [], OSTAP: [], LESYA: []}
 
-    # check availability of first argument & call openPacketsStack()
     try:
-        lines = openPacketsStack(sys.argv[1])
+        packets = open_logs(sys.argv[1])
     except IndexError:
         print(" [Problem] | You should enter name of file as first argument.")
         exit(1)
 
-    # call parsing packets
-    parsePacketReceiver(lines, receivers)
-    # saves data to files
-    dictToFile(receivers)
+    clasify_packets(packets, receivers)
+    packets_to_file(receivers)
 
     # unnecessary information. Just to see some result.
     print(" Sucessfully sorted.")
